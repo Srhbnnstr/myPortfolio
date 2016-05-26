56b6093c97c4d8510365a0643a0deb288c17ebf5
srhbnnstr <srhbnnstr@gmail.com>
2016-05-25 21:53:40 -0700

    ready for deploy

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-    $(".about").click(function() {
-        $(".overlay").fadeToggle(200);
-        $(".button a").toggleClass('btn-open').toggleClass('btn-close');
-    });
-});
-$('.button a').on('click', function() {
-    $(".overlay").fadeToggle(200);
-    $("button a").toggleClass('btn-open').toggleClass('btn-close');
-    open = false;
-});
+$(document).ready(function () {
+			  $(".navbar-toggle").on("click", function () {
+				    $(this).toggleClass("active");
+			  });
+		});
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-78278984-1', 'auto');
+  ga('send', 'pageview');
